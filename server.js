a7a9ab5d0f4fa3751ae2f2d259c8f9cdc8fd42df
godfreb <brandon.godfrey@autodesk.com>
2017-02-07 13:34:40 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var webSocketsServerPort = 1337;
+
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+var clients = []
+
+var server = http.createServer(function(request, response) {
+    // Not important for us. We're writing WebSocket server, not HTTP server
+});
+server.listen(webSocketsServerPort, function() {
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+});
+
+var wsServer = new webSocketServer({
+    httpServer: server
+});
+
+var num = 0;
+
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+    var connection = request.accept(null, request.origin); 
+    console.log((new Date()) + ' Connection accepted.');
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            
+			var msg_string = message.utf8Data
+			var split = msg_string.split(":");
+			
+            connection.id = split[0];
+            
+			if(split[1] == "connected"){
+				clients.push(connection)
+                
+                for (var i=0; i < clients.length-1; i++) {
+                    split[0] = clients[i].id;
+                    var concat = split.join(':');
+                    console.log(concat);
+                    clients[clients.length-1].sendUTF(concat);
+                }
+                
+			}
+			
+			// We simply rebroadcast all the messages and let the clients handle them
+			for (var i=0; i < clients.length; i++) {
+                // console.log(num++);
+				clients[i].sendUTF(msg_string);
+			}
+        }
+    });
+    connection.on('close', function(connection) {
+        //clients.splice(index, 1);
+    });
+
+});
